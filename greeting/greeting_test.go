f31f032f01da6e59f66e85bfbcff4b7aaf82c5e2
ak-sahli <ak-sahli@protonmail.com>
2017-03-10 17:42:13 +0100

    format greeting test

--- greeting/greeting_test.go
+++ greeting/greeting_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func TestGreeting(t *testing.T) {
 	expected := "Hello, World!"
-  observed := Greeting()
+	observed := Greeting()
 	if observed != expected {
-		t.Fatalf("HelloWorld() = %v, want %v", observed, expected)
+		t.Fatalf("observed %v, expected %v", observed, expected)
 	}
 }
